4cb586e11e8d0c7df2309d6f586036a38f91a72b
Jasiek Wolnicki <wolnickij@gmail.com>
2019-04-07 14:32:30 +0200

    Enum Country constructor and methods added

--- src/main/java/pl/sda/Country.java
+++ src/main/java/pl/sda/Country.java
@@ -1,8 +1,20 @@
 package pl.sda;
 
 public enum Country {
-    POLSKA,
-    NIEMCY,
-    CZECHY,
+    POLSKA ("Polska", "Poland"),
+    NIEMCY ("Niemcy", "Germany"),
+    CZECHY ("Czechy", "Czech Republic");
+
+    private final String countryPL;
+    private final String countryEN;
+
+    Country(String countryPL, String countryEN){
+        this.countryPL = countryPL;
+        this.countryEN = countryEN;
+    }
+
+    public String getCountryNamePL() {return countryPL;}
+    public String getCountryNameEN() {return countryEN;}
+
 
 }
